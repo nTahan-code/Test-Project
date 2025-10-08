65e5dd5ca8e2013a041c662d10b4f871799e6e43
Nick Tahan <ncth7t@mail.missouri.edu>
2025-10-08 10:07:41 -0500

    Add JavaDoc annotation

--- app/src/main/java/org/example/controller/HelloWorldController.java
+++ app/src/main/java/org/example/controller/HelloWorldController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    /**
+     * Hello World test endpoint
+     * @return {@link ResponseEntity} with 'Hello World' body, {@link org.springframework.http.HttpStatusCode} = <code>200</code>
+     */
     @GetMapping("/hello")
     public ResponseEntity<?> sayHello() {
         return ResponseEntity.ok("Hello World");
